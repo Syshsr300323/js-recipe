2a0c0612371b7366104f75c30a30d358c0d1a8b9
sakito_ishizawa <b2220250@photon.chitose.ac.jp>
2025-07-27 14:34:16 +0900

    aaaの追加

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,3 +1,4 @@
 const figure = document.getElementById("figure")
 const squareButton = document.getElementById("square-button")
 const circleButton = document.getElementById("circle-button")
+AuthenticatorAssertionResponse
